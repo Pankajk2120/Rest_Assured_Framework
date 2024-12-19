b12037302f5ac3d237b90099775e0e6d96e1c700
Pankajk2120 <khadepankaj30@gmail>
2024-12-20 00:02:56 +0530

    #6 : Create Reusable POST Utility As Part Of Framework

---
+++ src/test/java/Airlines/AirlineTest.java
@@ -0,0 +1,37 @@
+package Airlines;
+import java.util.HashMap;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import utils.RestUtils;
+
+public class AirlineTest {
+	
+	@Test
+	public void CreateAirline() {
+		
+		String endPoint = "https://api.instantwebtools.net/v1/airlines";
+		String payLoad = "{\r\n"
+				+ "    \"_id\": \"152d3bca-d9bb-476c-9a97-562d685e235c\",\r\n"
+				+ "    \"name\": \"Quatar Airways\",\r\n"
+				+ "    \"country\": \"Quatar\",\r\n"
+				+ "    \"logo\": \"https://upload.wikimedia.org/wikipedia/en/thumb/9/9b/Qatar_Airways_Logo.svg/300px-Qatar_Airways_Logo.svg.png\",\r\n"
+				+ "    \"slogan\": \"Going Places Together\",\r\n"
+				+ "    \"head_quaters\": \"Qatar Airways Towers, Doha, Qatar\",\r\n"
+				+ "    \"website\": \"www.qatarairways.com\",\r\n"
+				+ "    \"established\": \"1994\"\r\n"
+				+ "}\r\n"
+				+ "";
+		
+		
+		Response response = RestUtils.performPost(endPoint,payLoad,new HashMap<>());
+				
+				Assert.assertEquals(response.statusCode(), 200);
+				
+				System.out.println("response statusCode() : "+response.statusCode());
+	}
+
+}
