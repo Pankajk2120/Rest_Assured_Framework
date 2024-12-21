c42857997377e1b3110e8e1e84c0a27360fe794c
Pankajk2120 <khadepankaj30@gmail>
2024-12-22 01:00:26 +0530

    Print Formatted JSON & Headers As Table In Extent Report

--- src/main/java/RestUtils/RestUtils.java
+++ src/main/java/RestUtils/RestUtils.java
@@ -6,27 +6,54 @@ import org.testng.annotations.Test;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import io.restassured.specification.QueryableRequestSpecification;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.SpecificationQuerier;
+import reporting.ExtentReportManager;
 public class RestUtils {
 		
+	private static RequestSpecification getRequestSpecification(String endPoint, Object requestPayload, Map<String, String>headers) {
+		return RestAssured.given()
+				.baseUri(endPoint)
+				.headers(headers)
+				.contentType(ContentType.JSON)
+				.body(requestPayload);
+	}
+	
+	private static void printRequestLogInReport(RequestSpecification requestSpecification) {
+		QueryableRequestSpecification queryableRequestSpecification = SpecificationQuerier.query(requestSpecification);
+		ExtentReportManager.logInfoDetails("Endpoint is " + queryableRequestSpecification.getBaseUri());
+		ExtentReportManager.logInfoDetails("Method is " + queryableRequestSpecification.getMethod());
+		ExtentReportManager.logInfoDetails("Headers are ");
+		ExtentReportManager.logHeaders(queryableRequestSpecification.getHeaders().asList());
+		ExtentReportManager.logJson("Request body is ");
+		ExtentReportManager.logJson("Request body is " + queryableRequestSpecification.getBody());
+		
+	}
+	
+	private static void printResponseLogInReport(Response response) {
+		ExtentReportManager.logInfoDetails("Response Status is " + response.getStatusCode());
+		ExtentReportManager.logInfoDetails("Response Headers is");
+		ExtentReportManager.logHeaders(response.getHeaders().asList());
+		ExtentReportManager.logJson("Response body is");
+		ExtentReportManager.logJson("Response body is " + response.getBody().prettyPrint());
+	}
+	
 	public static Response performPost(String endPoint, String requestPayload, Map<String, String> headers) {
-	       return RestAssured.given().log().all()
-	       .baseUri(endPoint)
-	       .headers(headers)
-	       .contentType(ContentType.JSON)
-	       .body(requestPayload)
-	       .post()
-	       .then().log().all().extract().response();
+		RequestSpecification requestSpecification = getRequestSpecification(endPoint, requestPayload, headers);
+		Response response =  requestSpecification.post();
+		printRequestLogInReport(requestSpecification);
+		printResponseLogInReport(response);
+		return response;
 	}
 	
 	
 	public static Response performPost(String endPoint, Map<String,Object> requestPayload, Map<String, String> headers) {
-	       return RestAssured.given().log().all()
-	       .baseUri(endPoint)
-	       .headers(headers)
-	       .contentType(ContentType.JSON)
-	       .body(requestPayload)
-	       .post()
-	       .then().log().all().extract().response();
+		RequestSpecification requestSpecification = getRequestSpecification(endPoint, requestPayload, headers);
+		Response response =  requestSpecification.post();
+		printRequestLogInReport(requestSpecification);
+		printResponseLogInReport(response);
+		return response;
 	}
 	
 	
--- src/main/java/reporting/ExtentReportManager.java
+++ src/main/java/reporting/ExtentReportManager.java
@@ -2,10 +2,16 @@ package reporting;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.markuputils.CodeLanguage;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
+
+import io.restassured.http.Header;
 
 public class ExtentReportManager {
 		public static ExtentReports extentReports;
@@ -27,5 +33,30 @@ public class ExtentReportManager {
 	        String formattedTime = dateTimeFormatter.format(localDateTime);
 	        String reportName = "TestReport_" + formattedTime + ".html";
 	        return reportName;
+		}
+		
+		public static void logPassDetails(String log) {
+			Setup.extentTest.get().pass(MarkupHelper.createLabel(log, ExtentColor.GREEN));
+		}
+		
+		public static void logFailureDetails(String log) {
+			Setup.extentTest.get().fail(MarkupHelper.createLabel(log, ExtentColor.RED));
+		}
+		
+		public static void logInfoDetails(String log) {
+			Setup.extentTest.get().info(MarkupHelper.createLabel(log, ExtentColor.GREY));
+		}
+		
+		public static void logWarningDetails(String log) {
+			Setup.extentTest.get().warning(MarkupHelper.createLabel(log, ExtentColor.YELLOW));
+		}
+	
+		public static void logJson(String json) {
+			Setup.extentTest.get().info(MarkupHelper.createCodeBlock(json, CodeLanguage.JSON));
+		}
+		
+		public static void logHeaders(List<Header> headersList) {
+		    String[][] arrayHeaders = headersList.stream().map(header -> new String[] {header.getName(), header.getValue()}).toArray(String[][]::new);
+		    Setup.extentTest.get().info(MarkupHelper.createTable(arrayHeaders));
 		}
 }
